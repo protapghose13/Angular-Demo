822df9c439aee8f535ffcdd3ab0ef7be7f9dac38
enosis-protap <protap.ghosh@enosisbd.com>
2022-05-15 23:08:57 +0600

    Developed phone-cat app from seed project.

---
+++ PhoneCat/app/app.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.
+  module('phonecatApp').
+  config(['$routeProvider',
+    function config($routeProvider) {
+      $routeProvider.
+        when('/phones', {
+          template: '<phone-list></phone-list>'
+        }).
+        when('/phones/:phoneId', {
+          template: '<phone-detail></phone-detail>'
+        }).
+        when('/phonelisttable', {
+          template: '<phone-list-table></phone-list-table>'
+        }).
+        otherwise('/phones');
+    }
+  ]);
